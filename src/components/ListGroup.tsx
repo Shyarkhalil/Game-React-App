7c015616852e6e710d07af3d8e02fb35c83c038b
Shyar <shyr.khalil@gmail.com>
2024-09-11 12:39:44 +0200

    Remove handelClick function

--- src/components/ListGroup.tsx
+++ src/components/ListGroup.tsx
@@ -3,14 +3,13 @@ import React from 'react';
 function ListGroup() {
   const items = ['New York', 'Tokyo', 'Paris', 'Sydney', 'Cairo', 'Toronto'];
   type listItemClickEvent = React.MouseEvent<HTMLLIElement>;
-  const handelClick = (event: listItemClickEvent) => console.log(event);
   return (
     <>
       <h1>List</h1>
       {items.length === 0 && <h1>No items found</h1>}
       <ul className="list-group">
         {items.map((item) => (
-          <li key={nanoid()} className="list-group-item" onClick={handelClick}>
+          <li key={nanoid()} className="list-group-item active">
             {item}
           </li>
         ))}
