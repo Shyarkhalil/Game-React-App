5b38240ddb06551bb837033b2db151a8fc4437ec
Shyar <shyr.khalil@gmail.com>
2024-09-11 22:09:39 +0200

    Add Alert component

--- src/App.tsx
+++ src/App.tsx
@@ -1,12 +1,15 @@
 import './App.css';
+import Alert from './components/Alert';
 import './components/ListGroup';
 import ListGroup from './components/ListGroup';
 function App() {
   const items = ['New York', 'Tokyo', 'Paris', 'Sydney', 'Cairo', 'Toronto'];
   const cities = 'Cities';
+  const text = 'Alert';
   return (
     <div>
       <ListGroup items={items} heading={cities} />
+      <Alert text={text} />
     </div>
   );
 }
