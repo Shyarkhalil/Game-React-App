d3298fe2cd940e60ae6e7537d8156f06217f8ba0
Shyar <shyr.khalil@gmail.com>
2024-09-12 21:14:27 +0200

    Keep Static Data Outside the Componen

--- src/App.tsx
+++ src/App.tsx
@@ -2,20 +2,19 @@ import './App.css';
 import Alert from './components/Alert';
 import ButtonList from './components/ButtonList';
 import ListGroup from './components/ListGroup';
+const items = ['New York', 'Tokyo', 'Paris', 'Sydney', 'Cairo', 'Toronto'];
+const cities = 'Cities';
+const buttons = [
+  { className: 'btn btn-primary', name: 'Primary' },
+  { className: 'btn btn-secondary', name: 'Secondary' },
+  { className: 'btn btn-success', name: 'Success' },
+  { className: 'btn btn-danger', name: 'Danger' },
+  { className: 'btn btn-warning', name: 'Warning' },
+  { className: 'btn btn-info', name: 'Info' },
+  { className: 'btn btn-light', name: 'Light' },
+  { className: 'btn btn-dark', name: 'Dark' },
+];
 function App() {
-  const items = ['New York', 'Tokyo', 'Paris', 'Sydney', 'Cairo', 'Toronto'];
-  const cities = 'Cities';
-  const buttons = [
-    { className: 'btn btn-primary', name: 'Primary' },
-    { className: 'btn btn-secondary', name: 'Secondary' },
-    { className: 'btn btn-success', name: 'Success' },
-    { className: 'btn btn-danger', name: 'Danger' },
-    { className: 'btn btn-warning', name: 'Warning' },
-    { className: 'btn btn-info', name: 'Info' },
-    { className: 'btn btn-light', name: 'Light' },
-    { className: 'btn btn-dark', name: 'Dark' },
-  ];
-
   const handleButtonClick = (buttonName: string) => {
     console.log(`${buttonName} button clicked`);
   };
