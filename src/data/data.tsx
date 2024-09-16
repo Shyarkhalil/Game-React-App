ee431e62b577b8e9704d6cb954c86f2dbe89be5f
Shyar <shyr.khalil@gmail.com>
2024-09-16 14:14:15 +0200

    clean up the files

---
+++ src/data/data.tsx
@@ -0,0 +1,16 @@
+// Define the variables first
+const items = ['New York', 'Tokyo', 'Paris', 'Sydney', 'Cairo', 'Toronto'];
+const cities = 'Cities';
+const buttons = [
+  { className: 'btn btn-primary', name: 'Primary' },
+  { className: 'btn btn-secondary', name: 'Secondary' },
+  { className: 'btn btn-success', name: 'Success' },
+  { className: 'btn btn-danger', name: 'Danger' },
+  { className: 'btn btn-warning', name: 'Warning' },
+  { className: 'btn btn-info', name: 'Info' },
+  { className: 'btn btn-light', name: 'Light' },
+  { className: 'btn btn-dark', name: 'Dark' },
+];
+
+// Then export them all at once
+export { buttons, cities, items };
